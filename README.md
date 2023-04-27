ca59467862a4ee63ad102dc6bea55e9c93b69ff1
devctor <hxtr227@gmail.com>
2023-04-27 19:18:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# where-is-my-css
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qyiskx)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './reset.css'
+import './style.css';
+
+// Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
